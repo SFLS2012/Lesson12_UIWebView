a6ee41296c824d8fa0a88955bb3f7e00c1640c39
Harebert <harebert2008@163.com>
2013-03-18 19:21:26 +0800

    finish

--- Lesson12_UIWebView/ViewController.h
+++ Lesson12_UIWebView/ViewController.h
@@ -8,6 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIWebViewDelegate>
+- (IBAction)submit:(UIButton *)sender;
+@property (strong, nonatomic) IBOutlet UITextField *URLstring;
+@property (strong, nonatomic) IBOutlet UIWebView *myFirstWebView;
 
 @end
--- Lesson12_UIWebView/ViewController.m
+++ Lesson12_UIWebView/ViewController.m
@@ -13,9 +13,29 @@
 @end
 
 @implementation ViewController
-
+@synthesize myFirstWebView,URLstring;
 - (void)viewDidLoad
 {
+    //myFirstWebView=[[UIWebView alloc]init];
+    
+    NSString *webLinkString=@"http://www.apple.com.cn";
+    
+    NSURL *webLinkURL=[NSURL URLWithString:webLinkString];
+   
+    NSURLRequest *webLinkURLRequest=[NSURLRequest requestWithURL:webLinkURL ];
+    
+    [myFirstWebView loadRequest:webLinkURLRequest];
+    myFirstWebView.scalesPageToFit=YES;
+    UIWebView *mySecondWebView=[[UIWebView alloc]initWithFrame:CGRectMake(0, 274, 320, 274)];
+    
+    [self.view addSubview:mySecondWebView];
+    NSURL *secondWebLinkURL=[NSURL URLWithString:@"http://www.apple.com"];
+    
+    NSURLRequest *secondWebLinkURLRequest=[NSURLRequest requestWithURL:secondWebLinkURL];
+    
+    [mySecondWebView loadRequest:secondWebLinkURLRequest];
+    
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
@@ -26,4 +46,23 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)submit:(UIButton *)sender {
+    NSLog(@"%@",URLstring.text);
+    NSString *webLinkString=URLstring.text;
+    NSURL *webLinkURL=[NSURL URLWithString:webLinkString];
+    NSURLRequest *webLinkURLRequest=[NSURLRequest requestWithURL:webLinkURL ];
+    myFirstWebView.scalesPageToFit=YES;
+    [myFirstWebView loadRequest:webLinkURLRequest];
+    [URLstring resignFirstResponder];
+}
 @end
+
+
+
+
+
+
+
+
+
+
